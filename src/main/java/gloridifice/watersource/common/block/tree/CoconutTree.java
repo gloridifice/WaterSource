bbac0e318566d5fdf96f1f91d0b2be7a7f6e541b
Geolykt <admin@geolykt.de>
2020-12-21 00:16:32 +0100

    Removed CoconutTree compile errors
    While removing half of the functionallity the tree has.
    It's of course something that is fixable and as such will be fixed after it compiles
    and we can go into the testing phase of this. To be honest I don't think I am
    qualified enough to have made this patch, but who would have done it otherwise?
    
    Signed-off-by: Geolykt <admin@geolykt.de>

--- src/main/java/gloridifice/watersource/common/block/tree/CoconutTree.java
+++ src/main/java/gloridifice/watersource/common/block/tree/CoconutTree.java
@@ -1,52 +1,31 @@
 package gloridifice.watersource.common.block.tree;
 
-import com.google.common.collect.Sets;
-import gloridifice.watersource.common.world.gen.feature.CoconutTreeFeature;
 import gloridifice.watersource.registry.BlockRegistry;
 import gloridifice.watersource.registry.FeatureRegistry;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.trees.Tree;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.FeatureSpread;
+import net.minecraft.world.gen.feature.TwoLayerFeature;
 import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
-
+import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
 
 import javax.annotation.Nullable;
 import java.util.Random;
-import java.util.Set;
 
 public class CoconutTree extends Tree {
-    public static final TreeFeatureConfig COCONUT_TREE_CONFIG = (new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(BlockRegistry.BLOCK_COCONUT_TREE_LOG.getDefaultState()), new SimpleBlockStateProvider(BlockRegistry.BLOCK_COCONUT_TREE_LEAF.getDefaultState()), new BlobFoliagePlacer(2, 0))).baseHeight(4).heightRandA(2).foliageHeight(3).ignoreVines().setSapling((net.minecraftforge.common.IPlantable) BlockRegistry.BLOCK_COCONUT_SAPLING).build();
+    public static final BaseTreeFeatureConfig COCONUT_TREE_CONFIG = 
+            new BaseTreeFeatureConfig.Builder(
+                    new SimpleBlockStateProvider(BlockRegistry.BLOCK_COCONUT_TREE_LOG.getDefaultState()),
+                    new SimpleBlockStateProvider(BlockRegistry.BLOCK_COCONUT_TREE_LEAF.getDefaultState()),
+                    new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(1), 3),
+                    new StraightTrunkPlacer(7, 4, 2), // Base height, random height A, random height B
+                    new TwoLayerFeature(2, 0, 2)).setIgnoreVines().build();
 
     @Nullable
     @Override
-    protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean p_225546_2_) {
+    protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean p_225546_2_) {
         return FeatureRegistry.COCONUT_TREE.withConfiguration(COCONUT_TREE_CONFIG);
-    }
-
-    @Override
-    public boolean place(IWorld worldIn, ChunkGenerator<?> chunkGeneratorIn, BlockPos blockPosIn, BlockState blockStateIn, Random randomIn) {
-        ConfiguredFeature<TreeFeatureConfig, ?> configuredfeature = this.getTreeFeature(randomIn, true);
-        if (configuredfeature == null) {
-            return false;
-        } else {
-            Set<BlockPos> set = Sets.newHashSet();
-            Set<BlockPos> set1 = Sets.newHashSet();
-            MutableBoundingBox mutableboundingbox = MutableBoundingBox.getNewBoundingBox();
-            worldIn.setBlockState(blockPosIn, Blocks.AIR.getDefaultState(), 4);
-            (configuredfeature.config).forcePlacement();
-            if (((CoconutTreeFeature)configuredfeature.feature).place(worldIn,randomIn,blockPosIn,set,set1,mutableboundingbox,COCONUT_TREE_CONFIG)) {
-                return true;
-            } else {
-                worldIn.setBlockState(blockPosIn, blockStateIn, 4);
-                return false;
-            }
-        }
     }
 }
--- src/main/java/gloridifice/watersource/registry/FeatureRegistry.java
+++ src/main/java/gloridifice/watersource/registry/FeatureRegistry.java
@@ -1,10 +1,15 @@
 package gloridifice.watersource.registry;
 
-import gloridifice.watersource.common.world.gen.feature.CoconutTreeFeature;
+import gloridifice.watersource.WaterSource;
 import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
-import net.minecraft.world.gen.feature.Feature;;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.TreeFeature;;
 
 public class FeatureRegistry extends RegistryModule{
 
-    public static final Feature<BaseTreeFeatureConfig> COCONUT_TREE = new CoconutTreeFeature("coconut_tree", BaseTreeFeatureConfig::func_236685_a_);
+    public static final Feature<BaseTreeFeatureConfig> COCONUT_TREE;
+    static {
+        COCONUT_TREE = new TreeFeature(BaseTreeFeatureConfig.CODEC);
+        COCONUT_TREE.setRegistryName(WaterSource.MODID, "coconut_tree");
+    }
 }
