63e0580c9e556f29cde11bbfa2b72647e95f332b
Geolykt <admin@geolykt.de>
2020-12-21 14:27:38 +0100

    Fix biome init
    Now we can go to the main menu, cannot create a new world though because the datapacks appear to be bork
    
    Signed-off-by: Geolykt <admin@geolykt.de>

--- src/main/java/gloridifice/watersource/WaterSource.java
+++ src/main/java/gloridifice/watersource/WaterSource.java
@@ -5,8 +5,11 @@ import gloridifice.watersource.common.block.tree.CoconutTree;
 import gloridifice.watersource.common.network.SimpleNetworkHandler;
 import gloridifice.watersource.registry.*;
 import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -14,7 +17,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod("watersource")
 public class WaterSource {
@@ -33,14 +35,16 @@ public class WaterSource {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigRegistry.CLIENT_CONFIG);
         BlockRegistry.FLUID_BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ItemRegistry.FLUID_ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        MinecraftForge.EVENT_BUS.addListener(this::initBiomes);
     }
-    public void setup(FMLCommonSetupEvent event)
-    {
+    public void initBiomes(BiomeLoadingEvent event){
+        if (event.getName().equals(Biomes.BEACH.getRegistryName())){
+            event.getGeneration().withFeature(Decoration.VEGETAL_DECORATION, FeatureRegistry.COCONUT_TREE.withConfiguration(CoconutTree.COCONUT_TREE_CONFIG).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 0.5F, 1))));
+        }
+    }
+    public void setup(FMLCommonSetupEvent event){
         CapabilityRegistry.init();
         SimpleNetworkHandler.init();
-        // FIXME this is the wrong place for such registrations
-        // FIXME getFlowerFeatures might be the wrong category
-        ForgeRegistries.BIOMES.getValue(Biomes.BEACH.getLocation()).getGenerationSettings().getFlowerFeatures().add(FeatureRegistry.COCONUT_TREE.withConfiguration(CoconutTree.COCONUT_TREE_CONFIG).withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(0, 0.5F, 1))));
         RegisterManager.clearAll();
     }
     public void clientSetup(FMLClientSetupEvent event){
