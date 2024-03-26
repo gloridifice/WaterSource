7541077236917f8bfd31bd0aa03f81b4bb8b6260
Koiro <1165997495@qq.com>
2024-03-26 08:51:27 +0800

    refactor: move into WSConfig

---
+++ src/main/kotlin/xyz/koiro/watersource/WSConfig.kt
@@ -0,0 +1,63 @@
+package xyz.koiro.watersource
+
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+
+object WSConfig {
+    const val UNIT_DRINK_VOLUME = 250L //mB
+
+    //todo move other configs into this file
+    fun applyWaterLevelRestoreMultiplier(value: Int, mul: Int): Int{
+        return value * (1 + (mul - 1)/2)
+    }
+
+    object Exhaustion {
+        val SPRINT = 0.05f // per meter
+        val JUMP = 0.8f
+        val REWARD_HEALTH = 2f
+        fun thirstyPerSecond(amplifier: Int): Float{
+            return 0.5f + (amplifier + 1) * 0.3f
+        }
+        fun thirstyMultiplier(amplifier: Int): Float{
+            return 1.4f + amplifier.toFloat() * 0.4f
+        }
+    }
+
+    object Punishment {
+
+        private val DURATION = 50;
+        fun getPunishmentStatusEffectsSix(difficulty: WaterSource.ModDifficulty): Iterable<StatusEffectInstance>{
+            return when(difficulty){
+                WaterSource.ModDifficulty.PEACEFUL -> listOf()
+                WaterSource.ModDifficulty.EASY -> listOf()
+                WaterSource.ModDifficulty.NORMAL -> listOf(
+                    StatusEffectInstance(StatusEffects.SLOWNESS, DURATION),
+                    StatusEffectInstance(StatusEffects.WEAKNESS, DURATION)
+                )
+                WaterSource.ModDifficulty.HARD -> listOf(
+                    StatusEffectInstance(StatusEffects.SLOWNESS, DURATION, 1),
+                    StatusEffectInstance(StatusEffects.WEAKNESS, DURATION, 1)
+                )
+            }
+        }
+
+        fun getPunishmentStatusEffectsZero(difficulty: WaterSource.ModDifficulty): Iterable<StatusEffectInstance>{
+            return when(difficulty){
+                WaterSource.ModDifficulty.PEACEFUL -> listOf()
+                WaterSource.ModDifficulty.EASY -> listOf(
+                    StatusEffectInstance(StatusEffects.SLOWNESS, DURATION),
+                    StatusEffectInstance(StatusEffects.WEAKNESS, DURATION, 2)
+                )
+                WaterSource.ModDifficulty.NORMAL -> listOf(
+                    StatusEffectInstance(StatusEffects.SLOWNESS, DURATION, 1),
+                    StatusEffectInstance(StatusEffects.WEAKNESS, DURATION, 2)
+                )
+                WaterSource.ModDifficulty.HARD -> listOf(
+                    StatusEffectInstance(StatusEffects.SLOWNESS, DURATION, 2),
+                    StatusEffectInstance(StatusEffects.WEAKNESS, DURATION, 2)
+                )
+            }
+        }
+    }
+}
+
--- src/main/kotlin/xyz/koiro/watersource/WaterSourceConfig.kt
+++
@@ -1,5 +0,0 @@
-package xyz.koiro.watersource
-
-object WaterSourceConfig {
-    const val UNIT_DRINK_VOLUME = 250L //mB
-}
