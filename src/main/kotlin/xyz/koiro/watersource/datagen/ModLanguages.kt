98ecb47fef602275e0087598a06c063e1194453b
Koiro <1165997495@qq.com>
2024-06-09 14:30:44 +0800

    feat: add restoreAll command error message

--- src/main/kotlin/xyz/koiro/watersource/datagen/ModLanguages.kt
+++ src/main/kotlin/xyz/koiro/watersource/datagen/ModLanguages.kt
@@ -1,14 +1,18 @@
 package xyz.koiro.watersource.datagen
 
+import xyz.koiro.watersource.ModCommands
 import xyz.koiro.watersource.ModItemGroups
 import xyz.koiro.watersource.world.effect.ModStatusEffects
 
 object ModLanguages {
-    val INFO_CAPCITY_KYE = "watersource.info.capacity"
+    const val INFO_CAPACITY_KYE = "watersource.info.capacity"
     val list = listOf(
         Translation("Water Source", "水源", ModItemGroups.MAIN_ITEM_GROUP_TRANSLATION_KEY),
         Translation("Thirsty", "口渴", ModStatusEffects.THIRSTY.translationKey),
-        Translation("Capacity", "容量", INFO_CAPCITY_KYE),
+        Translation("Capacity", "容量", INFO_CAPACITY_KYE),
+
+        // Commands
+        Translation("<restoreAll> command's target must be player!", "<restoreAll> 命令的目标只能是玩家", ModCommands.RESTORE_ALL_TARGET_ERROR_TRANS_KEY),
     )
 
     data class Translation(
